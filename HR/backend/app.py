8b476f8496b5c1a7bde3faea7286f8a057d4d568
[Anushka Kulkarni] <[anushkaaa.08@gmail.com]>
2024-03-24 18:27:11 +0530

    final commit

--- HR/backend/app.py
+++ HR/backend/app.py
@@ -92,7 +92,7 @@ def users():
         for row in records:
             if row[4]:
                 skills = row[4][1:]
-            skills = skills.split(';')
+                skills = skills.split(';')
             user ={}
             user = {'id': row[0], 'name': row[1], 'email': row[2], 'mobile': row[3], 'skills': skills, 'experience': row[5], 'college_name': row[6], 'degree': row[7], 'designation': row[8], 'company': row[9], 'resume': row[10]}
             users.append(user)
